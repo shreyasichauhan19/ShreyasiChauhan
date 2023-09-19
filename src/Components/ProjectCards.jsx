5a4543327503c146163171ecf730f29f66175f71
Shreyasi Chauhan <chauhan.shreyasi19@gmail.com>
2023-09-19 16:58:23 -0700

    the prject cards are now aligned nicely

--- src/Components/ProjectCards.jsx
+++ src/Components/ProjectCards.jsx
@@ -1,7 +1,7 @@
 // ProjectCards.js
 import React from 'react';
 
-const ProjectCards = ({ title, imageSrc, link }) => {
+const ProjectCards = ({ title, imageSrc, description, link }) => {
   return (
     <div className="block rounded-lg bg-white shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)] dark:bg-neutral-700">
       <a href={link}>
@@ -15,7 +15,7 @@ const ProjectCards = ({ title, imageSrc, link }) => {
         <h5 className="mb-2 text-xl font-medium leading-tight text-neutral-800 dark:text-neutral-50">
           {title}
         </h5>
-        {/* Rest of the card content */}
+        <p>{description}</p>
       </div>
     </div>
   );
--- src/Pages/Projects.jsx
+++ src/Pages/Projects.jsx
@@ -7,37 +7,42 @@ const Projects = () => {
     {
       title: 'Web Scraping Robot',
       imageSrc: 'https://tecdn.b-cdn.net/img/new/standard/nature/184.jpg',
+      description: 'Hiiiiiiiiiiiiii',
       link: 'https://github.com/shreyasichauhan19/WebScrapingRobot',
     },
     {
       title: 'The Game of Amazons',
       imageSrc: 'https://tecdn.b-cdn.net/img/new/standard/nature/184.jpg',
+      description: 'Hiiiiiiiiiiiiii',
       link: 'https://github.com/shreyasichauhan19/COSC322_Project',
     },
     {
       title: 'Vernon Community Calendar',
       imageSrc: 'https://tecdn.b-cdn.net/img/new/standard/nature/184.jpg',
+      description: 'Hiiiiiiiiiiiiii',
       link: 'https://www.notion.so/shreyasic/Love-for-Vernon-Community-Calendar-UBC-Capstone-464fd99a7b3643388693616ecc5f5eef',
     },
     {
       title: 'Acronym Keyword Replacement',
       imageSrc: 'https://tecdn.b-cdn.net/img/new/standard/nature/184.jpg',
+      description: 'Hiiiiiiiiiiiiii',
       link: 'https://github.com/ThatOth3rGuy/Cosc320',
     },
   ];
 
   return (
     <div id="projects" className="w-full overflow-hidden-web flex justify-center">
-      <div className="w-0% min-h-screen relative mt-60 flex flex-col items-center justify-center pb-6">
-        <div className="w-80% h-[20%] mt-[15%] mb-[10%]">
+      <div className="w-full min-h-screen relative mt-60 flex flex-col items-center justify-center pb-6">
+        <div className="w-80% h-[20%] mt-[1%] mb-[5%]">
           <SectionTitle title="TECHNICAL PROJECTS" subtitle="What I have done so far" />
         </div>
         <div className="w-[80%] flex flex-wrap justify-center">
           {projectData.map((project, index) => (
-            <div className="w-[50%] h-[auto] m-4" key={index}>
+            <div className="w-[40%] h-[auto] m-4" key={index}>
               <ProjectCards
                 title={project.title}
                 imageSrc={project.imageSrc}
+                description={project.description}
                 link={project.link}
               />
             </div>
