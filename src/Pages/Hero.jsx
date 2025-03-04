f2c2db9577d94b20207279a81f403b101a09bdc2
Shreyasi Chauhan <chauhan.shreyasi19@gmail.com>
2025-03-04 02:02:31 -0500

    Landing page animation

--- src/Pages/Hero.jsx
+++ src/Pages/Hero.jsx
@@ -1,6 +1,5 @@
 import { m, LazyMotion, domAnimation } from "framer-motion";
 import HeroText from "../Components/HeroText";
-import HeroParticles from "../Components/Particles/HeroParticles";
 
 const Hero = () => {
   return (
@@ -11,7 +10,7 @@ const Hero = () => {
         className="relative w-full flex justify-center items-center h-screen min-h-[800px]"
       >
         <HeroText />
-        <HeroParticles />
+   
       </m.div>
     </LazyMotion>
     </div>
