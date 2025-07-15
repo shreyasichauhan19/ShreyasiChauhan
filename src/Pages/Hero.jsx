12c84b6c47e4f5f036029b1aaf6982b30cc8d089
Shreyasi Chauhan <chauhan.shreyasi19@gmail.com>
2025-07-14 20:10:47 -0400

    feat(ui): add Resume and GitHub buttons to landing page

--- src/Pages/Hero.jsx
+++ src/Pages/Hero.jsx
@@ -3,17 +3,55 @@ import HeroText from "../Components/HeroText";
 
 const Hero = () => {
   return (
+    <div>
     <div id="hero" className="w-full flex justify-center overflow-hidden-web relative">
     <LazyMotion features={domAnimation} strict>
       <m.div
         id="hero"
-        className="relative w-full flex justify-center items-center h-screen min-h-[800px]"
+        className="relative w-full flex justify-center items-center min-h-[800px]"
       >
         <HeroText />
    
       </m.div>
     </LazyMotion>
     </div>
+
+<div className="relative w-full flex justify-center mb-[5%] items-center gap-4">
+  <div>
+  <a
+    href="https://www.notion.so/shreyasic/Resume-3b7a6149c32340f28e8cf4e5e1ab2f5b"
+    target="_blank"
+    rel="noopener noreferrer"
+  >
+    <button
+      className="bg-primary-400 text-white px-6 py-3 rounded-lg shadow-md 
+      hover:bg-primary-700 transition duration-300"
+      style={{ fontFamily: "Quicksand" }}
+    >
+      Resume
+    </button>
+  </a>
+  </div>
+  <div>
+  <a
+    href="https://github.com/shreyasichauhan19"
+    target="_blank"
+    rel="noopener noreferrer"
+  >
+    <button
+      className="bg-primary-400 text-white px-6 py-3 rounded-lg shadow-md 
+      hover:bg-primary-700 transition duration-300"
+      style={{ fontFamily: "Quicksand" }}
+    >
+    Github
+    </button>
+  </a>
+  </div>
+</div>
+
+
+</div>
+
   );
 };
 
